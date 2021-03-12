320af696faa4a34c0beea2a6f8de8422a1c2cee2
Kami-code <im.B.C@live.com>
2021-03-13 01:23:37 +0800

    Add react route.

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,8 @@ import { DISHES } from '../shared/dishes';
 import DishDetail from "./DishdetailComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
+import Home from "./HomeComponent";
+import {Switch, Route, Redirect} from "react-router-dom";
 
 class Main extends Component {
 
@@ -12,23 +14,23 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
-            selectedDish: null
         };
     }
 
-    onDishSelect(dishId) {
-        this.setState({selectedDish: dishId});
-    }
-
     render() {
+        const HomePage = () => {
+            return (
+                <Home />
+            )
+        }
         return (
             <div>
                 <Header/>
-                <Menu dishes={this.state.dishes}
-                          onClick={(dishId) => this.onDishSelect(dishId)} />
-                <DishDetail
-                    dish={this.state.dishes.filter((dish) => dish.id === this.state.selectedDish)[0]}/>
-                {/*filter得到满足id = 选择的id的subarray*/}
+                <Switch>
+                    <Route path="/home" component={HomePage} />
+                    <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Redirect to="/home" />
+                </Switch>
                 <Footer/>
 
             </div>
