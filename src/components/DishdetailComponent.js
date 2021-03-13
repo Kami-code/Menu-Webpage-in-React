63c39e00def264e39e3be1624ce35bacd26e91d4
Kami-code <im.B.C@live.com>
2021-03-14 01:09:27 +0800

    Lesson 2 assignment. Add new pages and new components.

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle} from 'reactstrap'
+import {Card, CardImg, CardImgOverlay, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem} from 'reactstrap'
+import { Link } from 'react-router-dom'
 
 function RenderDish({dish}) {
     return (
@@ -22,7 +23,7 @@ function RenderComments({comments}) {
                 <li>{comment.comment}</li><br />
                 <li>--{comment.author},{new Intl.DateTimeFormat('en-US',{year:'numeric', month:'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}</li> <br />
             </div>
-    );
+        );
     });
     return (
         <div key = "comment" className="col-12 col-md-5 m-1">
@@ -39,10 +40,20 @@ const DishDetail = (props) => {
     else {
         return (
             <div className="container">
-                <div className="row">
-                    <RenderDish dish={props.dish} />
-                    <RenderComments comments={props.dish.comments}/>
-                </div>
+                    <div className="row">
+                        <Breadcrumb>
+                            <BreadcrumbItem><Link to='/menu'>Menu</Link></BreadcrumbItem>
+                            <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                        </Breadcrumb>
+                        <div className="col-12">
+                            <h3>{props.dish.name}</h3>
+                            <hr />
+                        </div>
+                    </div>
+                    <div className="row">
+                        <RenderDish dish={props.dish} />
+                        <RenderComments comments={props.comments}/>
+                    </div>
             </div>
         );
     }
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -2,11 +2,16 @@ import React, { Component } from "react";
 import Menu from "./MenuComponents";
 import '../App.css';
 import { DISHES } from '../shared/dishes';
+import { COMMENTS } from "../shared/comments";
+import { LEADERS } from "../shared/leaders";
+import { PROMOTIONS } from "../shared/promotions";
 import DishDetail from "./DishdetailComponent";
+import Contact from "./ContactComponent";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
 import Home from "./HomeComponent";
 import {Switch, Route, Redirect} from "react-router-dom";
+import About from "./AboutComponent";
 
 class Main extends Component {
 
@@ -14,21 +19,38 @@ class Main extends Component {
         super(props);
         this.state = {
             dishes: DISHES,
+            comments: COMMENTS,
+            promotion: PROMOTIONS,
+            leaders: LEADERS
         };
     }
 
     render() {
         const HomePage = () => {
             return (
-                <Home />
+                <Home dish={this.state.dishes.filter((dish)=> dish.featured)[0]}
+                    promotion={this.state.promotion.filter((promo) => promo.featured)[0]}
+                      leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+                />
             )
         }
+
+        const DishWithId = ({match}) =>{
+            return (
+                <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]}
+                    comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))}
+                />
+            );
+        };
         return (
             <div>
                 <Header/>
                 <Switch>
                     <Route path="/home" component={HomePage} />
                     <Route exact path="/menu" component={() => <Menu dishes={this.state.dishes}/>}/>
+                    <Route path="/menu/:dishId" component={DishWithId} />
+                    <Route exact path="/contactus" component={Contact} />
+                    <Route exact path="/aboutus" component={() => <About leaders={this.state.leaders}/>}/>
                     <Redirect to="/home" />
                 </Switch>
                 <Footer/>
